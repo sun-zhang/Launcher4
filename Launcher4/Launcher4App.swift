5f82093366930ae4a89f528e613236d5020bfa9c
zhangsen <zs178@163.com>
2026-02-21 09:52:24 +0800

    Initial Commit

---
+++ Launcher4/Launcher4App.swift
@@ -0,0 +1,17 @@
+//
+//  Launcher4App.swift
+//  Launcher4
+//
+//  Created by Sam Zhang on 2026/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Launcher4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
